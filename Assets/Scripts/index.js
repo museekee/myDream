e63b5f8c326b9362621fa93e9ebff688a4c2b8e9
museekee <103117239+museekee@users.noreply.github.com>
2022-12-13 23:21:25 +0900

    사진자료 늘림

--- Assets/Scripts/index.js
+++ Assets/Scripts/index.js
@@ -6,10 +6,14 @@ const elems = {
 }
 
 for (const elem of document.getElementsByTagName("port-post")) {
-    elem.addEventListener("click", (e) => {
+    elem.addEventListener("mousedown", (e) => {
         if (e.currentTarget instanceof Element) {
             elems.maindialog.back.style.display = "flex"
             elems.maindialog.dialog.innerHTML = e.currentTarget.innerHTML
         }
     })
 }
+
+elems.maindialog.back.addEventListener("mousedown", e => {
+    elems.maindialog.back.style.display = "none"
+})
